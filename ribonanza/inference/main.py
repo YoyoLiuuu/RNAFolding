b82c23dc6875ea65485c596f9869e95bc52c04d7
YoyoLiuuu <yoyoyimeng.liu@mail.utoronto.ca>
2025-03-04 19:06:54 -0500

    ribonanza - working progress

---
+++ ribonanza/inference/main.py
@@ -0,0 +1,13 @@
+from dataset import RNA2D_set
+from model import finetuned_RibonanzaNet
+from model import load_config_from_yaml
+import pandas as pd 
+
+test_data = pd.read_csv('data/casp15.csv')
+test_dataset = RNA2D_set(test_data)
+
+print(test_dataset[0])
+
+model = finetuned_RibonanzaNet(load_config_from_yaml("ribonanzanet2d/configs/pairwise.yaml")).cuda()
+
+print(model)
