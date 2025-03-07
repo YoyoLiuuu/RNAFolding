1497b21712132f0d7c8aa6da45afddab17ce1117
YoyoLiuuu <yoyoyimeng.liu@mail.utoronto.ca>
2025-03-07 03:45:30 +0000

    created main.py corresponding to casp15.py

--- ribonanza/inference/dataset.py
+++ ribonanza/inference/dataset.py
@@ -11,7 +11,7 @@ from torch.utils.data import Dataset, DataLoader
 
 class RNA2D_set(Dataset): 
     def __init__(self, data): 
-        self.data = data 
+        self.data = pd.DataFrame(data) # convert input to DF
         self.tokens = {nt:i for i, nt in enumerate('ACGU')}
     
     def __len__(self):
